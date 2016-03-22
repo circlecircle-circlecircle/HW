be1ce7a6bf019c389e71a7448b5fc2039812fe37
circlecircle-circlecircle <aa333536@yahoo.com.tw>
2016-03-22 22:14:51 +0800

    第三次作業~
    完工

--- 0322doHw.R
+++ 0322doHw.R
@@ -1,46 +1,18 @@
----
-title: "NBA 2014-2015球季 各隊分析"
-output: github_document
----
-
-``` {r echo=T}
-#install.packages("SportsAnalytics")
 library(SportsAnalytics)
 NBA1415<-fetch_NBAPlayerStatistics("14-15")
-```
 
-## 各隊最辛苦的球員
-
-我的程式碼:
-
-``` {r echo=T}
-HardestPerson<-aggregate(TotalMinutesPlayed~Team,NBA1415,max)
-NBA1415Hardest<-merge(NBA1415,HardestPerson)
-output<-NBA1415Hardest[order(NBA1415Hardest$TotalMinutesPlayed,decreasing = T),c("Team","Name","TotalMinutesPlayed")]
-library(knitr)
-kable(output, digits=2)
-```
-
-## 各隊得分王
-
-計算依據全季總得分最多的球員，我的程式碼:
-
-```{r echo=T}
 MaxPoint<-aggregate(TotalPoints~Team,NBA1415,max)
-#tapply(NBA1415$TotalPoints,NBA1415$Team,max)
 NBA1415MaxPoint<-merge(NBA1415,MaxPoint)
 output<-NBA1415MaxPoint[order(NBA1415MaxPoint$TotalPoints,decreasing = T),c("Team","Name","TotalPoints")]
 library(knitr)
 kable(output, digits=2)
 
-```
+HardestPerson<-aggregate(TotalMinutesPlayed~Team,NBA1415,max)
+NBA1415Hardest<-merge(NBA1415,HardestPerson)
+output<-NBA1415Hardest[order(NBA1415Hardest$TotalMinutesPlayed,decreasing = T),c("Team","Name","TotalMinutesPlayed")]
+library(knitr)
+kable(output, digits=2)
 
-
-## 各隊最有效率的球員
-
-我的程式碼:
-
-```{r echo=T}
 EffectivePlayer<-aggregate(TotalPoints/TotalMinutesPlayed~Name,NBA1415,max)
 NBA1415EffectivePlayer<-merge(NBA1415,EffectivePlayer)
 EffectivePlayerTeam<-aggregate(TotalPoints/TotalMinutesPlayed~Team,NBA1415EffectivePlayer,max)
@@ -48,14 +20,7 @@ NBA1415EffectivePlayerTeam<-merge(NBA1415EffectivePlayer,EffectivePlayerTeam)
 output<-NBA1415EffectivePlayerTeam[order(NBA1415EffectivePlayerTeam$`TotalPoints/TotalMinutesPlayed`,decreasing = T),c("Team","Name","TotalPoints/TotalMinutesPlayed")]
 library(knitr)
 kable(output, digits=2)
-```
 
-
-## 各隊三分球出手最準的球員
-
-我的程式碼:
-
-```{r echo=T}
 ThreeMadeName<-aggregate(ThreesMade/ThreesAttempted~Name,NBA1415,max)
 NBA1415Name<-merge(NBA1415,ThreeMadeName)
 ThreeMadeTeam<-aggregate(ThreesMade/ThreesAttempted~Team,NBA1415,max)
@@ -63,7 +28,3 @@ NBA1415Team<-merge(NBA1415Name,ThreeMadeTeam)
 output<-NBA1415Team[order(NBA1415Team$`ThreesMade/ThreesAttempted`,decreasing = T),c("Team","Name","ThreesMade/ThreesAttempted")]
 library(knitr)
 kable(output, digits=2)
-```
-
-
-
